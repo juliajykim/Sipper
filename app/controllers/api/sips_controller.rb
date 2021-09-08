aef7b7d04034f4cc944ca105970cdf9fbc68ecfc
juliajykim <jkim24@gwu.edu>
2021-09-08 02:40:42 -0400

    modify controller

--- app/controllers/api/sips_controller.rb
+++ app/controllers/api/sips_controller.rb
@@ -1,15 +1,22 @@
-class Api::SipsController < ApplicationController 
-    def index
-        @sips = Sip.all.includes(:author) 
-        render :index
-    end
+class Api::SipsController < ApplicationController
+  def index
+    @sips = Sip.all.includes(:author)
+    render :index
+  end
 
-    def create
+  def create
+    @sip = Sip.new(sip_params)
+    @sip.author = User.first
 
+    if @sip.save
+      render :show
+    else
+      render json: @sip.errors.full_messages, status: 422
     end
+  end
 
-    def show
-        @sip = Sip.find_by(id: params[:id])
-        render :show
-    end
+  def show
+    @sip = Sip.find_by(id: params[:id])
+    render :show
+  end
 end
